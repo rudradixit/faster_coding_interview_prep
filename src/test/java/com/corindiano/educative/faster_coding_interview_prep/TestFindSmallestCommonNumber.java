0dbfa2f48ac450e0bc04b41edf0fa3e304b0ae3a
Rudra A Dixit <rudradixit@gmail.com>
2018-10-08 16:30:19 -0400

    Solved Find Smallest Common Number

--- src/test/java/com/corindiano/educative/faster_coding_interview_prep/TestFindSmallestCommonNumber.java
+++ src/test/java/com/corindiano/educative/faster_coding_interview_prep/TestFindSmallestCommonNumber.java
@@ -13,4 +13,13 @@ class TestFindSmallestCommonNumber {
 
         assertEquals(30, FindSmallestCommonNumber.find_least_common_number(a, b, c));
     }
+
+    @Test
+    void testCase2() {
+        int[] a = new int[]{1, 6, 10, 14, 50};
+        int[] b = new int[]{-1, 6, 7, 8, 50};
+        int[] c = new int[]{0, 6, 7, 10, 25, 30, 40};
+
+        assertEquals(6, FindSmallestCommonNumber.find_least_common_number(a, b, c));
+    }
 }
